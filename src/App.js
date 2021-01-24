423fd7a3489667cb3fbddf3ed168fd5e6c7f1d22
Julia <yulyakonoval1998@gmail.com>
2021-01-24 18:44:03 +0200

    carousel component was styled

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import SignIn from "./SignIn";
+import DemoCarousel from "./DemoCarousel";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <DemoCarousel />
       <SignIn />
     </div>
   );
