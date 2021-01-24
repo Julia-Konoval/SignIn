78938b5c9809c16817e5a64bebfd035c7dfb43e5
Julia <yulyakonoval1998@gmail.com>
2021-01-24 14:21:46 +0200

    form was created, logo applied

---
+++ src/SignIn.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import logo from "./images/logo.PNG";
+
+export default function SignIn() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
+  console.log(watch("example")); // watch input value by passing the name of it
+  //   validateEmail(email){}
+  //   handleChange(e){}
+  return (
+    <div>
+      <div className="logo">
+        {/* <img src={logo} alt="Logo" /> */}
+        <p>MERGE DEVELOPMENT</p>
+      </div>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Sign Up</h1>
+
+        <label>Email</label>
+        <input
+          name="email"
+          ref={register({
+            required: true,
+            minLength: 4,
+            pattern: /[a-zA-Z0-9]+[\.]?([a-zA-Z0-9]+)?[\@][a-z]{3,9}[\.][a-z]{2,5}/g,
+          })}
+        />
+        {errors.email && errors.email.type === "required" && (
+          <p>Email is required</p>
+        )}
+
+        <label>Password</label>
+        <input
+          name="password"
+          ref={register({
+            required: true,
+            maxLength: 17,
+          })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>Password is required</p>
+        )}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
